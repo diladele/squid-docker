e94cf7c6ec7ebb7078a5c682b2ac91b6cceb6ef7
Diladele <support@diladele.com>
2016-11-08 00:52:17 +0100

    Some changes in the entry point according to the recommendation for official packages

--- src/contents/entrypoint.sh
+++ src/contents/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -e
 
-echo "Starting squid..."
-exec $(which squid) -NCd 1
+# allow passing arguments
+if [[ ${1:0:1} = '-' ]]; then
+  EXTRA_ARGS="$@"
+  set --
+elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
+  EXTRA_ARGS="${@:2}"
+  set --
+fi
+
+# default behaviour is to launch squid
+if [[ -z ${1} ]]; then
+  echo "Launching squid..."
+  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+else
+  exec "$@"
+fi
